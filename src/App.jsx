bd7e51ed4e902c5601e8b0c01c24ef88c63a0510
O-BaianoOficial007 <arthurandradepereira7@gmail.com>
2026-05-20 12:42:17 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -9,58 +9,70 @@ function App() {
     <>
       <nav>
         <span className={s.navItems} onclick="navTo('s1')">
-            <img src="../assets/icons/home.png" alt="Jogo" width="30px"/>
+            <img src="/home.png" alt="Jogo" width="30px"/>
                 <a className={s.navWorlds}>Ínicio</a>
         </span>
          <span className={s.navItems} onclick="navTo('s2')">
-            <img src="../assets/icons/taça.png" alt="Carros"  width="30px"/>
+            <img src="/taça.png" alt="Carros"  width="30px"/>
             <a className={s.navWorlds} >Títulos</a>
         </span>
          <span className={s.navItems}onclick="navTo('s3')">
-            <img src="../assets/icons/jogador.png" alt="Vídeo"  width="30px"/>
+            <img src="/jogador.png" alt="Vídeo"  width="30px"/>
             <a className={s.navWorlds}>Nossos Jogadores</a>
         </span>
          <span className={s.navItems}onclick="navTo('s4')">
-            <img src="../assets/icons/mobile.png" alt="Contato"  width="30px"/>
+            <img src="/mobile.png" alt="Contato"  width="30px"/>
             <a className={s.navWorlds}>Contato</a>
         </span>
     </nav>
     <main>
-        <section id="s1">
+        <section className={s.s1}>
            <h1>Flamengo</h1>
            <h2>Uma vez Flamengo, sempre Flamengo!!!</h2>
         </section>
-        <section id="s2">
-            <h3>QUEM SOMOS</h3>
+             <h3>QUEM SOMOS</h3>
+        <section id="s2" className={s.s2}>
+       
             <div className={s.card}>
                 <div>
                     <h4>Libertadores</h4>
-                    <img src={liberta} alt=""></img>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas vitae sed modi, obcaecati rerum voluptas aliquam fugit sint facere laudantium, numquam nihil at ipsum, ad consectetur quia assumenda. In, similique.</p>
+                    <img src='/liberta.jpg' alt="" width={200} height={200}></img>
+                    <p>Tetracampeões: 1981; 2019; 2022; 2025</p>
                 </div>
             </div>
             <div className={s.card}>
                   
                 <div>
                     <h4>Brasileirão</h4>
-                    <img src={br} alt=""/>
-                    <p></p>
+                    <img src='/brasileirão.jpg' alt="" width={200} height={200}/>
+                    <p>Eneacampeões: 1980; 1982; 1983; 1987; 1992; 2009; 2019; 2020; 2025</p>
                 </div>
             </div>
-            <div classname={s.card}>
+            <div className={s.card}>
                 
                 <div>
                     <h4>Copa do Brasil</h4>
-                    <img src={cdb}alt=""/>
-                    <p></p>
+                    <img src='/cdb.jpg' alt="" width={200} height={200}/>
+                    <p>Pentacampeões: 1990 (invicto); 2006; 2013; 2022; 2024</p>
                 </div>
             </div>
         </section>
-        <section id="s3">
+        <section id="s3" className={s.s3}>
             <iframe class="video" width="1900" height="1000" src="https://www.youtube.com/embed/LxFRxMrTXLg?si=3XeiRZ9FtIeNwrb8autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </section>
-        <section id="s4">
-            <h2></h2>
+        <section id="s4" className={s.s4}>
+            <h3>CONTATO</h3>
+      
+        <div className={s.formulario}>
+          <label for="nome">Nome:</label>
+          <input type="text" id="nome" placeholder="Digite seu nome"/>
+          <label for="assunto">Mensagem:</label>
+          <textarea id="assunto" placeholder="Digite uma mensagem"></textarea>
+          <button onclick="enviar()">ENVIAR</button>
+        </div>
+        <div>
+          <div id="saturno-container"></div>
+        </div>
         </section>
     </main>
     <footer>
